18a97bbd7fe6d9ea2ac5136e726108001cff2576
hYe807n <rlagkdus55@gmail.com>
2023-12-06 15:51:57 +0900

    test: 매칭 이력이 있는 레벨이라면, true 반환

---
+++ src/test/java/pairmatching/model/MatcherTest.java
@@ -0,0 +1,32 @@
+package pairmatching.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MatcherTest {
+
+    @DisplayName("페어 매칭 결과와, 레벨 명을 통해 저장된 결과 pairs 가 일치")
+    @Test
+    void matchPairs() {
+        Matcher matcher = new Matcher();
+        Assertions.assertEquals(matcher.matchPairs("백엔드,레벨1,자동차경주"),
+            matcher.getPairsByLevel("백엔드,레벨1,자동차경주"));
+    }
+
+    @DisplayName("매칭 이력이 없는 레벨 검색 시, 에러 발생")
+    @Test
+    void noneMatchPairs() {
+        Matcher matcher = new Matcher();
+        Assertions.assertThrows(IllegalArgumentException.class,
+            () -> matcher.getPairsByLevel("백엔드,레벨1,자동차경주"));
+    }
+
+    @DisplayName("매칭 이력이 있는 레벨이라면, true 반환")
+    @Test
+    void isAlreadyMatched() {
+        Matcher matcher = new Matcher();
+        matcher.matchPairs("백엔드,레벨1,자동차경주");
+        Assertions.assertTrue(matcher.isDuplicatedLevel("백엔드,레벨1,자동차경주"));
+    }
+}
