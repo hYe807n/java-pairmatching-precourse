de16d1762149137323ade06a311598da6d4317cc
hYe807n <rlagkdus55@gmail.com>
2023-12-06 14:34:50 +0900

    test: 백엔드 크루가 백/프론트인지 확인하기 위해 비교하는 함수를 통해 true/false 반환

--- src/test/java/pairmatching/model/CrewTest.java
+++ src/test/java/pairmatching/model/CrewTest.java
@@ -1,18 +1,28 @@
 package pairmatching.model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import pairmatching.enums.Course;
 
 class CrewTest {
 
-    @DisplayName("백엔드 크루가 백엔드인지 확인하기 위해 비교하는 함수를 통해 true 반환")
-    @Test
-    void isSameCourse() {
-        Crew crew = new Crew(Course.BACKEND, "하연");
-        Assertions.assertTrue(crew.isSameCourse(Course.BACKEND));
+    private static Stream<Arguments> compareCourse() {
+        return Stream.of(
+            Arguments.arguments(Course.BACKEND, "백엔드", true),
+            Arguments.arguments(Course.FRONTEND, "프론트엔드", false)
+        );
+    }
+
+    public static Crew initializeCrew() {
+        return new Crew(Course.BACKEND, "하연");
+    }
+
+    @ParameterizedTest(name = "백엔드 크루가 {1}인지 확인하기 위해 비교하는 함수를 통해 {2} 반환")
+    @MethodSource("compareCourse")
+    void isSameCourse(Course course, String courseName, boolean value) {
+        Assertions.assertEquals(initializeCrew().isSameCourse(course), value);
     }
 }
