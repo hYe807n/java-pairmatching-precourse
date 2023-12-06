f65b61377f639c8993a651fbd94632d1cbbd8595
hYe807n <rlagkdus55@gmail.com>
2023-12-06 14:19:10 +0900

    test: 페어가 홀수라면 마지막 페어가 3 명으로 구성

--- src/test/java/pairmatching/model/PairsTest.java
+++ src/test/java/pairmatching/model/PairsTest.java
@@ -11,10 +11,21 @@ class PairsTest {
     @DisplayName("pairs 가 담은 pair 에 \"하연\"crew 를 담음")
     @Test
     void pairsOfPair() {
-        Crew crewHaYeon = new Crew(Course.BACKEND, "하연");
-        Crew crewYeonHa = new Crew(Course.BACKEND, "연하");
+        Crew crewHaYeon = new Crew(Course.BACKEND, "A");
+        Crew crewYeonHa = new Crew(Course.BACKEND, "B");
         Pairs pairs = new Pairs("백엔드,레벨1,자동차경주",
             Arrays.asList(crewHaYeon, crewYeonHa));
         Assertions.assertEquals(pairs.getPairs().get(0).getCrews().get(0), crewHaYeon);
+    }
+
+    @DisplayName("페어가 홀수라면 마지막 페어가 3 명으로 구성")
+    @Test
+    void pairsByOddNumber() {
+        Crew crewA = new Crew(Course.BACKEND, "A");
+        Crew crewB = new Crew(Course.BACKEND, "B");
+        Crew crewC = new Crew(Course.BACKEND, "C");
+        Pairs pairs = new Pairs("백엔드,레벨1,자동차경주",
+            Arrays.asList(crewA, crewB, crewC));
+        Assertions.assertEquals(pairs.getPairs().get(0).getCrews(), Arrays.asList(crewA, crewB, crewC));
     }
 }
